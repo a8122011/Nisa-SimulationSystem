36919fa02444c0732ee546a94839a9416f92dd40
a8122011 <axxdzn412@gmail.com>
2025-11-18 16:07:16 +0900

    Add HomeController for asset formation simulation

---
+++ main/java/simulateAssetFormationWithNISA/HomeController.java
@@ -0,0 +1,12 @@
+package java.simulateAssetFormationWithNISA;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.util.*;
