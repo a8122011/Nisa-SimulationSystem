f305618dd25bbfebca9b0f6d6d355cd17be3088a
a8122011 <axxdzn412@gmail.com>
2025-11-18 16:05:19 +0900

    Create SimulateAssetFormationWithNisaApplication class

---
+++ main/java/simulateAssetFomationWithNisaApplication.java
@@ -0,0 +1,14 @@
+package jp.java.voyage.simulateAssetFormationWithNISA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulateAssetFormationWithNisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulateAssetFormationWithNisaApplication.class, args);
+
+	}
+
+}
