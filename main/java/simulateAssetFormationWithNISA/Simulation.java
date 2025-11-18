c5bab3a976edb55df8e6420ec54fabfd6292a4fd
a8122011 <axxdzn412@gmail.com>
2025-11-18 16:11:42 +0900

    Change package name for Simulation.java

---
+++ main/java/simulateAssetFormationWithNISA/Simulation.java
@@ -0,0 +1,11 @@
+package java/SimulateAssetFormationWithNISA;
+
+import jp.java.voyage.simulateAssetFormationWithNISA.HomeController.SimulationParams;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+
+
+public class Simulation {
+    static int simuNum = 10000;
