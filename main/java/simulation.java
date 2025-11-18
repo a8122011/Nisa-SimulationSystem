24958f4b25bb3bb5b18c52eda58143b2ab43fd2b
a8122011 <axxdzn412@gmail.com>
2025-11-18 15:58:55 +0900

    Create Simulation class in simulation.java
    Add initial structure for Simulation class

---
+++ main/java/simulation.java
@@ -0,0 +1,11 @@
+package jp.java;
+
+import jp.java.voyage.simulateAssetFormationWithNISA.HomeController.SimulationParams;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+
+
+public class Simulation {
+    static int simuNum = 10000;
