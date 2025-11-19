45bf1939d760e98da7559397679f3622ada3f45a
a8122011 <axxdzn412@gmail.com>
2025-11-19 22:33:39 +0900

    Add LifeEventStrValidation class for error handling

---
+++ main/java/simulateAssetFormationWithNISA/LifeEventStrValidation.java
@@ -0,0 +1,57 @@
+package jp.java.voyage.simulateAssetFormationWithNISA;
+
+public class lifeEventValidation {
+    String lifeEventAge1Error;
+    String requiredFunds1Error;
+    String lifeEventAge2Error;
+    String requiredFunds2Error;
+    String lifeEventAge3Error;
+    String requiredFunds3Error;
+    String lifeEventAge4Error;
+    String requiredFunds4Error;
+    String lifeEventAge5Error;
+    String requiredFunds5Error;
+
+    public lifeEventValidation() {
+    }
+
+    public void setLifeEventAge1Error(String lifeEventAge1Error) {
+        this.lifeEventAge1Error = lifeEventAge1Error;
+    }
+
+    public void setRequiredFunds1Error(String requiredFunds1Error) {
+        this.requiredFunds1Error = requiredFunds1Error;
+    }
+
+    public void setLifeEventAge2Error(String lifeEventAge2Error) {
+        this.lifeEventAge2Error = lifeEventAge2Error;
+    }
+
+    public void setRequiredFunds2Error(String requiredFunds2Error) {
+        this.requiredFunds2Error = requiredFunds2Error;
+    }
+
+    public void setLifeEventAge3Error(String lifeEventAge3Error) {
+        this.lifeEventAge3Error = lifeEventAge3Error;
+    }
+
+    public void setRequiredFunds3Error(String requiredFunds3Error) {
+        this.requiredFunds3Error = requiredFunds3Error;
+    }
+
+    public void setLifeEventAge4Error(String lifeEventAge4Error) {
+        this.lifeEventAge4Error = lifeEventAge4Error;
+    }
+
+    public void setRequiredFunds4Error(String requiredFunds4Error) {
+        this.requiredFunds4Error = requiredFunds4Error;
+    }
+
+    public void setLifeEventAge5Error(String lifeEventAge5Error) {
+        this.lifeEventAge5Error = lifeEventAge5Error;
+    }
+
+    public void setRequiredFunds5Error(String requiredFunds5Error) {
+        this.requiredFunds5Error = requiredFunds5Error;
+    }
+}
