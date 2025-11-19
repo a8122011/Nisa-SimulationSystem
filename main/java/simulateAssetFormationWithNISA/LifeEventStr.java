1f0eb30411581dedb9d08b207ad431c8d35e2ae1
a8122011 <axxdzn412@gmail.com>
2025-11-19 22:19:08 +0900

    Create LifeEventStr data class for life events

---
+++ main/java/simulateAssetFormationWithNISA/LifeEventStr.java
@@ -0,0 +1,28 @@
+package jp.java.voyage.simulateAssetFormationWithNISA;
+
+//ライフイベントの年齢と必要資金をまとめたデータクラス
+public class lifeEventStr {
+    String lifeEventAge1;
+    String requiredFunds1;
+    String lifeEventAge2;
+    String requiredFunds2;
+    String lifeEventAge3;
+    String requiredFunds3;
+    String lifeEventAge4;
+    String requiredFunds4;
+    String lifeEventAge5;
+    String requiredFunds5;
+
+    public lifeEventStr(String lifeEventAge1, String requiredFunds1, String lifeEventAge2, String requiredFunds2, String lifeEventAge3, String requiredFunds3, String lifeEventAge4, String requiredFunds4, String lifeEventAge5, String requiredFunds5) {
+        this.lifeEventAge1 = lifeEventAge1;
+        this.requiredFunds1 = requiredFunds1;
+        this.lifeEventAge2 = lifeEventAge2;
+        this.requiredFunds2 = requiredFunds2;
+        this.lifeEventAge3 = lifeEventAge3;
+        this.requiredFunds3 = requiredFunds3;
+        this.lifeEventAge4 = lifeEventAge4;
+        this.requiredFunds4 = requiredFunds4;
+        this.lifeEventAge5 = lifeEventAge5;
+        this.requiredFunds5 = requiredFunds5;
+    }
+}
